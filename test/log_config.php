43af8342e6b327eb38ed7ce5f9ffa76c758c4c61
Ben Leung <ben_leungut@yahoo.com>
2015-07-08 15:58:12 +0800

    Converted test file to use spaces instead of tabs, as per @fuxu's comment.

--- test/log_config.php
+++ test/log_config.php
@@ -1,15 +1,15 @@
 <?php
 //日志配置
 $config = array(
-	'log_path' => 'output/',
-	'product'  => 'uc',
-	'level'    => 3,
-	'path'     => array(
-		'FATAL' => 'php/php',
-		'RPC'   => 'rpc/rpc',
-		'SYS'   => 'sys/sys',
-	),
-	'suffix'  => array(
-		'WARNING' => '.wf',
-	),
+    'log_path' => 'output/',
+    'product'  => 'uc',
+    'level'    => 3,
+    'path'     => array(
+        'FATAL' => 'php/php',
+        'RPC'   => 'rpc/rpc',
+        'SYS'   => 'sys/sys',
+    ),
+    'suffix'  => array(
+        'WARNING' => '.wf',
+    ),
 );
